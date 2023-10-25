f1a645de2b414f2b1bcdd0dbd51a8d0199d16f15
Gabriel Alves <alvesg2003@gmail.com>
2023-10-24 23:01:27 -0300

    Solucionando Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("00 00000-0000", "tijolao", "123123123", 16);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Wild Rift");
+
+        Smartphone iphone = new Iphone("00 00000-0000", "11", "123123123", 16);
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Crossfire");
+    }
+}
